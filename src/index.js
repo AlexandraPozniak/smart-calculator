dff0053be77865bdcaa77d2d5ad330ca8f205546
AlexandraPozniak <32781040+AlexandraPozniak@users.noreply.github.com>
2018-10-08 13:57:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.number = eval(initialValue);
+  }
+  toString(){
+    return eval(this.number);
   }
 
   add(number) {
+    this.number += "+" + number;
+    return this;
     // your implementation
   }
-  
+
   subtract(number) {
+    this.number += "-" + number;
+    return this;
     // your implementation
   }
 
   multiply(number) {
+    this.number+= "*" + number;
+    return this;
     // your implementation
   }
 
   devide(number) {
+    this.number += "/" + number;
+    return this;
     // your implementation
   }
 
   pow(number) {
+    this.number += "**" + number;
+    return this;
     // your implementation
   }
 }
+this.number = eval(this.number);
 
 module.exports = SmartCalculator;
+
